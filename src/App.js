e36f8898a65d4ad8ffbc6099e786aa539f56ce1c
Fredrik Johan Dahlbom <fredrik.dahlbom@cmeducations.se>
2022-04-10 09:23:38 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
+import Post from './Post';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() { // = PostList.js
+  const [posts, setPosts] = useState([]);
+
+    // Runs when componentDidMount, componentDidUpdate, componentWillUnmount
+    useEffect( async () => {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const data = await response.json();
+            console.log(data);
+            
+            setPosts(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }, []) // An empty array, ensures that useEffect only runs when componentDidMount
+
+    return (
+        <div>
+            {
+                posts.map(post => <Post key={post.id} post={post}/>) 
+            }
+        </div>
+    )
 }
 
 export default App;
